74e6fa01d3ff89d7c6401518cc622c322f14094f
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-08-22 00:17:14 +0530

    polyfill for some

---
+++ array/some.js
@@ -0,0 +1,8 @@
+Array.prototype.ownSome = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i])) {
+            return true;
+        }
+    }
+    return false;
+};
